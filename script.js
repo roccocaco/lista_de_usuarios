a5871c7b2ab6842571d1db1c9bf6c677a79ef2f6
Rocco Caco <32roccocaco@gmail.com>
2025-01-31 16:54:36 -0300

    updated

---
+++ script.js
@@ -0,0 +1,48 @@
+// Imports
+
+// Seletores
+const inputName = document.getElementById('input-name');
+const inputCity = document.getElementById('input-city');
+const btnSearch = document.getElementById('btn-search');
+const divContainer = document.getElementById('container');
+
+// Variaveis
+let results = [];
+
+// Funções
+const fetchApi = async () => {
+    const url = "https://jsonplaceholder.typicode.com/users";
+    const req = await fetch(url);
+    const res = await req.json();
+
+    return res
+};
+
+// Eventos
+window.addEventListener('load', async () => {
+    results = await fetchApi();
+
+    results.map((e) =>
+        divContainer.innerHTML += `
+            <h4>Nome: ${e.name}</h4>
+            <p>Email: ${e.email}<p>
+            <p>Cidade: ${e.address.city}<p>
+    `
+    );
+});
+
+btnSearch.addEventListener('click', () => {
+    const userName = inputName.Value;
+    const userCity = inputCity.Value;
+
+    const filterByName = results.filter((e) => e.name === userName);
+
+    divContainer.innerHTML = "";
+    filterByName.map((e) =>
+        divContainer.innerHTML += `
+            <h4>Nome: ${e.name}</h4>
+            <p>Email: ${e.email}<p>
+            <p>Cidade: ${e.adress.city}<p>
+    `
+    );
+});
