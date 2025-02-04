3c7f3ed4fb70973d77ce2ba0cfb421528d1799d8
Rocco Caco <32roccocaco@gmail.com>
2025-02-04 19:43:15 -0300

    update

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ const fetchApi = async () => {
     const url = "https://jsonplaceholder.typicode.com/users";
     const req = await fetch(url);
     const res = await req.json();
+    console.log(res)
 
     return res
 };
@@ -36,6 +37,34 @@ btnSearch.addEventListener('click', () => {
     const userCity = inputCity.Value;
 
     const filterByName = results.filter((e) => e.name === userName);
+    const filterByCity = results.filter((e) => e.adress.city === userCity);
+
+    if (filterByName.length === 0 && userCity === "") {
+        return divContainer.innerHTML = `
+            <p>Não existe um usuário com esse nome.</p>
+        `;
+    };
+
+    if (filterByCity.length === 0 && userName === "") {
+        return divContainer.innerHTML = `
+            <p>Não existe uma cidade com esse nome.</p>
+        `;
+    };
+
+    if (filterByName.length === 0 && filterByCity.length === 0) {
+        return divContainer.innerHTML = `
+            <p>Não existe uma cidade e usuário com esse nome.</p>
+        `;
+    };
+
+    divContainer.innerHTML = "";
+    filterByCity.map((e) =>
+        divContainer.innerHTML += `
+            <h4>Nome: ${e.name}</h4>
+            <p>Email: ${e.email}</p>
+            <p>Cidade: ${e.address.city}</p>
+    `
+    );
 
     divContainer.innerHTML = "";
     filterByName.map((e) =>
