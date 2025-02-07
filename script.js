6bb258b50600d1cdc1e36c7512c758e3cb91b5b0
Rocco Caco <32roccocaco@gmail.com>
2025-02-07 20:03:01 -0300

    update

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 const inputName = document.getElementById('input-name');
 const inputCity = document.getElementById('input-city');
 const btnSearch = document.getElementById('btn-search');
+const btnName = document.getElementById('btn-name');
 const divContainer = document.getElementById('container');
 
 // Variaveis
@@ -14,8 +15,7 @@ const fetchApi = async () => {
     const url = "https://jsonplaceholder.typicode.com/users";
     const req = await fetch(url);
     const res = await req.json();
-    console.log(res)
-
+    
     return res
 };
 
@@ -32,42 +32,38 @@ window.addEventListener('load', async () => {
     );
 });
 
-btnSearch.addEventListener('click', () => {
-    const userName = inputName.Value;
-    const userCity = inputCity.Value;
-
+btnName.addEventListener('click', () => {
+    const userName = inputName.value;
     const filterByName = results.filter((e) => e.name === userName);
-    const filterByCity = results.filter((e) => e.adress.city === userCity);
 
-    if (filterByName.length === 0 && userCity === "") {
+    if (filterByName.length === 0) {
         return divContainer.innerHTML = `
             <p>Não existe um usuário com esse nome.</p>
         `;
     };
 
-    if (filterByCity.length === 0 && userName === "") {
-        return divContainer.innerHTML = `
-            <p>Não existe uma cidade com esse nome.</p>
-        `;
-    };
-
-    if (filterByName.length === 0 && filterByCity.length === 0) {
-        return divContainer.innerHTML = `
-            <p>Não existe uma cidade e usuário com esse nome.</p>
-        `;
-    };
-
     divContainer.innerHTML = "";
-    filterByCity.map((e) =>
+    filterByName.map((e) =>
         divContainer.innerHTML += `
             <h4>Nome: ${e.name}</h4>
             <p>Email: ${e.email}</p>
             <p>Cidade: ${e.address.city}</p>
     `
     );
+})
+
+btnSearch.addEventListener('click', () => {    
+    const userCity = inputCity.value;
+    const filterByCity = results.filter((e) => e.address.city === userCity);
+
+    if (filterByCity.length === 0) {
+        return divContainer.innerHTML = `
+            <p>Não existe uma cidade com esse nome.</p>
+        `;
+    };
 
     divContainer.innerHTML = "";
-    filterByName.map((e) =>
+    filterByCity.map((e) =>
         divContainer.innerHTML += `
             <h4>Nome: ${e.name}</h4>
             <p>Email: ${e.email}</p>
